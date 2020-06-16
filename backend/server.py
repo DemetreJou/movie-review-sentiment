c549155e39938998231b5556ea2e0892fcb471e3
DemetreJou <demetre.jouras@gmail.com>
2020-06-15 22:56:55 -0400

    setup flask to serve static frontend

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, send_from_directory
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+app = Flask(__name__, static_folder='../frontend/build')
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/taylor")
 def baby():
     return "<h1> You're baby !! <3 </h1>"
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
