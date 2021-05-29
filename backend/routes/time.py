d8e2517d34d7ea11f6e933346acade7bdb23c6e3
demetre <demetre.jouras@gmail.com>
2021-05-29 12:47:18 -0400

    update backend routes

--- backend/routes/time.py
+++ backend/routes/time.py
@@ -1,7 +1,7 @@
 import time
-from flask import Flask
 from flask_cors import cross_origin
 from . import routes
+
 
 @routes.route('/api/v1/time')
 @cross_origin()
--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,5 @@
 from flask import Flask
 from routes import *
-import keras
-import os
-
-# this path should probably come through as an environment variable for difference when in docker container vs when local dev
-model = keras.models.load_model(os.path.join("..", "sentiment_analysis", "trained_model"))
 
 app = Flask(__name__)
 app.register_blueprint(routes)
