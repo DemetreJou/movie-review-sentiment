f446e78d754dbf482ff3375729dc61f129ef19e5
DemetreJou <demetre.jouras@gmail.com>
2021-09-01 23:22:45 -0400

    add retry logic with exponential backoff

--- backend/routes/sentiment_analysis.py
+++ backend/routes/sentiment_analysis.py
@@ -2,11 +2,15 @@ from flask import jsonify, request
 import requests
 from flask_cors import cross_origin
 from . import routes
+from requests_toolbelt import sessions
+from utils.network_request import request_client_generator
 
+
+request_instance = request_client_generator(base_url="http://localhost:5100/api/v1/")
 
 @routes.route('/api/v1/get_sentiment', methods=['GET'])
 @cross_origin()
 def get_sentiment():
     phrase = request.args.get("phrase")
-    response = requests.get("http://localhost:5100/api/v1/get_sentiment?phrase=" + phrase)
+    response = request_instance.get("http://localhost:5100/api/v1/get_sentiment?phrase=" + phrase, timeout=10)
     return response.json()
